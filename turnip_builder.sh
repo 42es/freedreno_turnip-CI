068318732693ad601c1c8b1d4eda4cf6dd6c003d
42es <134020101+42es@users.noreply.github.com>
2026-02-06 16:24:27 +0800

    Update turnip_builder.sh

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -197,8 +197,8 @@ EOF
 
 port_lib_for_adrenotool(){
 	echo "Using patchelf to match soname ..."  $'\n'
-	cp "$workdir"/mesa/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so "$workdir"
-	cd "$workdir"
+	sudo cp "$workdir"/mesa/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so "$workdir"
+	sudo cd "$workdir"
 	patchelf --set-soname vulkan.adreno.so libvulkan_freedreno.so
 	mv libvulkan_freedreno.so vulkan.ad07XX.so
 
