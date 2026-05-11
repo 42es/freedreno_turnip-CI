2a7da126589a41a70acc5586a678429509144436
42es <134020101+42es@users.noreply.github.com>
2026-05-12 06:53:42 +0800

    Update commit link format in turnip_gen8.sh

--- turnip_gen8.sh
+++ turnip_gen8.sh
@@ -256,7 +256,7 @@ EOF
 		echo "Turnip - $mesa_version - vk$vulkan_version - $date" > release
 		echo "$mesa_version"_"$commit_short" > tag
 		echo  $filename > filename
-		echo "### Base commit : [$commit_short](https://gitlab.freedesktop.org/mesa/mesa/-/commit/$commit_short)" > description
+		echo "### Base commit : [$commit_short](https://github.com/lfdevs/mesa-for-android-container/src//mesa/-/commit/$commit_short)" > description
 		echo "false" > patched
 		echo "false" > experimental
 	else		
