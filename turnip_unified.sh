f2f825ce987f7fc9e4fd0b83a8e910af184a78f2
42es <134020101+42es@users.noreply.github.com>
2026-05-12 09:41:24 +0800

    Update mesa source URLs for Turnip branch

---
+++ turnip_unified.sh
@@ -0,0 +1,294 @@
+#!/bin/bash -e
+green='\033[0;32m'
+red='\033[0;31m'
+nocolor='\033[0m'
+
+deps="meson ninja unzip curl pip flex bison zip git"
+workdir="$(pwd)/turnip_workdir"
+packagedir="$workdir/turnip_module"
+ndkver="android-ndk-r29"
+sdkver="36"
+cver="35"
+mesasrc="https://github.com/whitebelyash/mesa-unified/tree/turnip/gen8"
+driver="vulkan.turnip.so"
+
+#array of string => commit/branch;patch args
+#base_patches=(
+#		"vk;merge_requests/38323;"
+#		"wsi_syncobj;merge_requests/31149;"
+#		"tu_direct;merge_requests/38960;"
+#		"vk_barrier;merge_requests/38956;"
+#		"wsi_immediate;merge_requests/40584;"
+#		"wsi_fix;merge_requests/41188;"
+        #'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
+#)
+#experimental_patches=(
+#		"tu_ubwc;merge_requests/39491;"
+        #"copy_raw;merge_requests/35610;"
+        #"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
+#)
+#failed_patches=()
+#commit=""
+#commit_short=""
+#mesa_version=""
+#vulkan_version=""
+#clear
+
+# there are 4 functions here, simply comment to disable.
+# you can insert your own function and make a pull request.
+run_all(){
+	check_deps
+	prep
+
+	if (( ${#base_patches[@]} )); then
+		prep "patched"
+	fi
+ 
+	if (( ${#experimental_patches[@]} )); then
+		prep "experimental"
+	fi
+}
+
+prep () {
+	prepare_workdir "$1"
+	build_lib_for_android
+	port_lib_for_adrenotool "$1"
+}
+
+check_deps(){
+	sudo apt remove meson
+	pip install meson PyYAML
+
+	echo "Checking system for required Dependencies ..."
+	for deps_chk in $deps;
+		do
+			sleep 0.25
+			if command -v "$deps_chk" >/dev/null 2>&1 ; then
+				echo -e "$green - $deps_chk found $nocolor"
+			else
+				echo -e "$red - $deps_chk not found, can't countinue. $nocolor"
+				deps_missing=1
+			fi;
+		done
+
+		if [ "$deps_missing" == "1" ]
+			then echo "Please install missing dependencies" && exit 1
+		fi
+
+	echo "Installing python Mako dependency (if missing) ..." $'\n'
+	pip install mako &> /dev/null
+}
+
+prepare_workdir(){
+	echo "Creating and entering to work directory ..." $'\n'
+	mkdir -p "$workdir" && cd "$_"
+
+	if [ -z "${ANDROID_NDK_LATEST_HOME}" ]; then
+		if [ ! -n "$(ls -d android-ndk*)" ]; then
+			echo "Downloading android-ndk from google server (~640 MB) ..." $'\n'
+			curl https://dl.google.com/android/repository/"$ndkver"-linux.zip --output "$ndkver"-linux.zip &> /dev/null
+			###
+			echo "Exracting android-ndk to a folder ..." $'\n'
+			unzip "$ndkver"-linux.zip  &> /dev/null
+		fi
+	else	
+		echo "Using android ndk from github image"
+	fi
+
+	if [ -z "$1" ]; then
+		if [ -d mesa-unified ]; then
+			echo "Removing old mesa-unified ..." $'\n'
+			rm -rf mesa-unified
+		fi
+		
+		echo "Cloning mesa-unified ..." $'\n'
+		git clone --depth=100 "$mesasrc" "$workdir/mesa-unified"
+		cd mesa-unified
+		commit_short=$(git rev-parse --short HEAD)
+		commit=$(git rev-parse HEAD)
+		mesa_version=$(cat VERSION | xargs)
+		version=$(awk -F'COMPLETE VK_MAKE_API_VERSION(|)' '{print $2}' <<< $(cat include/vulkan/vulkan_core.h) | xargs)
+		major=$(echo $version | cut -d "," -f 2 | xargs)
+		minor=$(echo $version | cut -d "," -f 3 | xargs)
+		patch=$(awk -F'VK_HEADER_VERSION |\n#define' '{print $2}' <<< $(cat include/vulkan/vulkan_core.h) | xargs)
+		vulkan_version="$major.$minor.$patch"
+	else		
+		cd mesa-unified
+
+		if [ $1 == "patched" ]; then 
+			apply_patches ${base_patches[@]}
+		else 
+			apply_patches ${experimental_patches[@]}
+		fi
+		
+	fi
+}
+
+apply_patches() {
+	local arr=("$@")
+	for patch in "${arr[@]}"; do
+		echo "Applying patch $patch"
+		patch_source="$(echo $patch | cut -d ";" -f 2 | xargs)"
+		patch_args=$(echo $patch | cut -d ";" -f 3 | xargs)
+		if [[ $patch_source == *"../.."* ]]; then
+			if git apply $patch_args "$patch_source"; then
+				echo "Patch applied successfully"
+			else
+				echo "Failed to apply $patch"
+				failed_patches+=("$patch")
+
+			fi
+		else 
+			patch_file="${patch_source#*\/}"
+			curl --output "../$patch_file".patch -k --retry-delay 30 --retry 5 -f --retry-all-errors https://github.com/whitebelyash/mesa-unified/tree/turnip/gen8/src/mesa/-/"$patch_source".patch
+			sleep 1
+
+			if git apply $patch_args "../$patch_file".patch ; then
+				echo "Patch applied successfully"
+			else
+				echo "Failed to apply $patch"
+				failed_patches+=("$patch")
+				
+			fi
+		fi
+	done
+}
+
+patch_to_description() {
+	local arr=("$@")
+	for patch in "${arr[@]}"; do
+		patch_name="$(echo $patch | cut -d ";" -f 1 | xargs)"
+		patch_source="$(echo $patch | cut -d ";" -f 2 | xargs)"
+		patch_args="$(echo $patch | cut -d ";" -f 3 | xargs)"
+		if [[ $patch_source == *"../.."* ]]; then
+			echo "- $patch_name, $patch_source, $patch_args" >> description
+		else 
+			echo "- $patch_name, [$patch_source](https://github.com/whitebelyash/mesa-unified/src/mesa/-/$patch_source), $patch_args" >> description
+		fi
+	done
+}
+
+build_lib_for_android(){
+	echo "Creating meson cross file ..." $'\n'
+	if [ -z "${ANDROID_NDK_LATEST_HOME}" ]; then
+		ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
+		ndk_sys="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
+	else	
+		ndk="$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin"
+		ndk_sys="$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
+	fi
+	
+    [ ! -f "$ndk/aarch64-linux-android${cver}-clang" ] && cver="34"
+	
+	cat <<EOF >"android-aarch64"
+[binaries]
+ar = '$ndk/llvm-ar'
+c = ['ccache', '$ndk/aarch64-linux-android$cver-clang', '--sysroot=$ndk_sys']
+cpp = ['ccache', '$ndk/aarch64-linux-android$cver-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments', '--sysroot=$ndk_sys']
+c_ld = '$ndk/ld.lld'
+cpp_ld = '$ndk/ld.lld'
+strip = '$ndk/llvm-strip'
+[host_machine]
+system = 'android'
+cpu_family = 'aarch64'
+cpu = 'armv8'
+endian = 'little'
+EOF
+
+	echo "Generating build files ..." $'\n'
+	meson setup build-android-aarch64 --cross-file "$workdir"/mesa-unified/android-aarch64 \
+ 		-Dbuildtype=release \
+   		-Dplatforms=android \
+     	-Dplatform-sdk-version=$sdkver \
+       	-Dandroid-stub=true \
+		-Degl=disabled \
+	 	-Dgallium-drivers= \
+  		-Dvulkan-drivers=freedreno \
+		-Dfreedreno-kmds=kgsl \
+  	 	-Dvulkan-beta=true &> "$workdir"/meson_log
+        
+
+	echo "Compiling build files ..." $'\n'
+	ninja -C build-android-aarch64 &> "$workdir"/ninja_log
+}
+
+port_lib_for_adrenotool(){
+	cp "$workdir"/mesa-unified/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so "$workdir"/"$driver"
+	cd "$workdir"
+
+	#if ! [ -a "$driver" ]; then
+	#	echo -e "$red Build failed! $nocolor" && exit 1
+	#fi
+
+	mkdir -p "$packagedir" && cd "$_"
+
+	date=$(date +'%b %d, %Y')
+	suffix=""
+
+	if [ ! -z "$1" ]; then
+		suffix="_$1"
+	fi
+
+	cat <<EOF >"meta.json"
+{
+  "schemaVersion": 1,
+  "name": "Turnip - $mesa_version - $date - $commit_short$suffix - Gen8",
+  "description": "Compiled from Mesa, Commit $commit_short$suffix",
+  "author": "mesa",
+  "packageVersion": "1",
+  "vendor": "Mesa",
+  "driverVersion": "$mesa_version/vk$vulkan_version",
+  "minApi": 28,
+  "libraryName": "$driver"
+}
+EOF
+
+	filename=Turnip_"$mesa_version"_"vk$vulkan_version"_"$(date +'%b-%d-%Y')"_"$commit_short"_Gen8
+	echo "Copy necessary files from work directory ..." $'\n'
+	cp "$workdir"/"$driver" "$packagedir"
+
+	echo "Packing files in to adrenotool package ..." $'\n'
+	zip -9 "$workdir"/"$filename$suffix".zip ./*
+
+	cd "$workdir"
+
+	if [ -z "$1" ]; then
+		echo "Turnip - $mesa_version - vk$vulkan_version - $date" > release
+		echo "$mesa_version"_"$commit_short" > tag
+		echo  $filename > filename
+		echo "### Base commit : [$commit_short](https://github.com/whitebelyash/mesa-unified/tree/turnip/gen8/src//mesa/-/commit/$commit_short)" > description
+		echo "false" > patched
+		echo "false" > experimental
+	else		
+		if [ $1 == "patched" ]; then 
+			echo "## Upstreams / Patches" >> description
+			echo "These have not been merged by Mesa officially yet and may introduce bugs or" >> description
+			echo "we revert stuff that breaks games but still got merged in (see --reverse)" >> description
+			patch_to_description ${base_patches[@]}
+			echo "true" > patched
+			echo "" >> description
+			echo "_Upstreams / Patches are only applied to the patched version (\_patched.zip)_" >> description
+			echo "_If a patch is not present anymore, it's most likely because it got merged, is not needed anymore or was breaking something._" >> description
+		else 
+			echo "### Upstreams / Patches (Experimental)" >> description
+			echo "Include previously listed patches + experimental ones" >> description
+			patch_to_description ${experimental_patches[@]}
+			echo "true" > experimental
+			echo "" >> description
+			echo "_Experimental patches are only applied to the experimental version (\_experimental.zip)_" >> description
+		fi
+	fi
+
+	if (( ${#failed_patches[@]} )); then
+		echo "" >> description
+		echo "#### Patches that failed to apply" >> description
+		patch_to_description ${failed_patches[@]}
+	fi
+	
+	if ! [ -a "$workdir"/"$filename".zip ];
+		then echo -e "$red-Packing failed!$nocolor" && exit 1
+		else echo -e "$green-All done, you can take your zip from this folder;$nocolor" && echo "$workdir"/
+	fi
+}
+
+run_all
