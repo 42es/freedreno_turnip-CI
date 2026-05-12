2e5430bdeb1304d13e8e306d26de0b09a6fc1c26
42es <134020101+42es@users.noreply.github.com>
2026-05-12 17:44:31 +0800

    Update turnip_DiskDVD.sh

--- turnip_DiskDVD.sh
+++ turnip_DiskDVD.sh
@@ -233,8 +233,8 @@ port_lib_for_adrenotool(){
 {
   "schemaVersion": 1,
   "name": "Turnip - $mesa_version - $date - $commit_short$suffix - Gen8",
-  "description": "Compiled from Mesa, Commit $commit_short$suffix",
-  "author": "mesa",
+  "description": "Compiled from Mesa(https://github.com/DiskDVD/A8XX-Y), Commit $commit_short$suffix",
+  "author": "DiskDVD",
   "packageVersion": "1",
   "vendor": "Mesa",
   "driverVersion": "$mesa_version/vk$vulkan_version",
